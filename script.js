c51d2e96b4e5e9a63761003a6b3935b44ddc74da
TheRogueTownTRT <158230961+TheRogueTownTRT@users.noreply.github.com>
2024-02-06 16:28:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ function executeCommand(command) {
     // Check the command and provide a response
     switch (command.toLowerCase()) {
         case '/collection':
-            terminalOutput.innerHTML += '<p>There are 333 NFTs in the collection.</p>';
+            terminalOutput.innerHTML += '<p>There are 100 NFTs in the collection.</p>';
             break;
         case '/discord':
             terminalOutput.innerHTML += '<p>Here is the official Discord link: <a href="https://discord.gg/P6aeYwKMsw" target="_blank">https://discord.gg/P6aeYwKMsw</a></p>';
+            break;
+        case '/commands':
+            displayCommands();
             break;
         default:
             terminalOutput.innerHTML += '<p>Command not recognized. Please try again.</p>';
@@ -31,4 +34,15 @@ function executeCommand(command) {
 
     // Scroll to the bottom of the terminal output
     terminalOutput.scrollTop = terminalOutput.scrollHeight;
+}
+
+// Function to display available commands
+function displayCommands() {
+    terminalOutput.innerHTML += '<p>Available commands:</p>';
+    terminalOutput.innerHTML += '<ul>';
+    terminalOutput.innerHTML += '<li>/collection - Gives the amount of NFTs in the collection</li>';
+    terminalOutput.innerHTML += '<li>/discord - Gives the official Discord link</li>';
+    terminalOutput.innerHTML += '<li>/commands - Shows available commands</li>';
+    // Add more commands as needed
+    terminalOutput.innerHTML += '</ul>';
 }
